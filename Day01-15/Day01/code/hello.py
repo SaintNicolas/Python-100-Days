7b8d45e9afb881693ad3ef494c50abf2f5ff629f
ch <chenpengfei@cqu.edu.cn>
2019-05-19 02:08:43 +0800

    更改

--- Day01-15/Day01/code/hello.py
+++ Day01-15/Day01/code/hello.py
@@ -13,5 +13,6 @@ python hello.py
 print('hello, world!')
 # print("你好,世界！")
 print('你好', '世界')
+print('goodbye, world')
 print('hello', 'world', sep=', ', end='!')
 print('goodbye, world', end='!\n')
